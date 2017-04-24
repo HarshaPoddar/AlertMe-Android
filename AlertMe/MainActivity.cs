ec359dc00453697113cd2f0978f8b4df350e9c6a
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-04-24 15:38:02 +0530

    GPS Configured!

--- AlertMe/MainActivity.cs
+++ AlertMe/MainActivity.cs
@@ -5,13 +5,46 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Util;
+using System.Threading.Tasks;
+using Java.Lang;
 
 namespace AlertMe
 {
     [Activity(Label = "AlertMe", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, ILocationListener
     {
-        int count = 1;
+        static readonly string TAG = "X:" + typeof(MainActivity).Name;
+        TextView _addressText;
+        Location _currentLocation;
+        LocationManager _locationManager;
+
+        string _locationProvider;
+        TextView _locationText;
+
+        public async void OnLocationChanged(Location location)
+        {
+            _currentLocation = location;
+            if (_currentLocation == null)
+            {
+                _locationText.Text = "Unable to determine your location. Try again in a short while.";
+            }
+            else
+            {
+                _locationText.Text = "Latitude"+_currentLocation.Latitude.ToString() +" Longitude:-"+ _currentLocation.Longitude.ToString();
+                Address address = await ReverseGeocodeCurrentLocation();
+                DisplayAddress(address);
+            }
+        }
+
+        public void OnProviderDisabled(string provider) { }
+
+        public void OnProviderEnabled(string provider) { }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras) { }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,12 +53,87 @@ namespace AlertMe
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            _addressText = FindViewById<TextView>(Resource.Id.AdressTextBox);
+            _locationText = FindViewById<TextView>(Resource.Id.location_text);
+            FindViewById<ImageButton>(Resource.Id.LocationImageButton).Click += LocationImageButton_OnClick;
+            InitializeLocationManager();
         }
+
+        void InitializeLocationManager()
+        {
+            _locationManager = (LocationManager)GetSystemService(LocationService);
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Medium
+            };
+            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+            if(acceptableLocationProviders.Any())
+            {
+                _locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                _locationProvider = string.Empty;
+            }
+            Log.Debug(TAG, "Using" + _locationProvider + ".");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (_locationManager.IsProviderEnabled(LocationManager.NetworkProvider))
+            {
+                _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+            }
+            _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _locationManager.RemoveUpdates(this);
+        }
+
+        async void LocationImageButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if(_currentLocation == null)
+            {
+                _addressText.Text = "Cant determine the location of the device! Please try again later.";
+                return;
+            }
+            Address address = await ReverseGeocodeCurrentLocation();
+            DisplayAddress(address);
+        }
+
+        async Task<Address> ReverseGeocodeCurrentLocation()
+        {
+            Geocoder geocoder = new Geocoder(this);
+            IList<Address> AddressList = await geocoder.GetFromLocationAsync(_currentLocation.Latitude, _currentLocation.Longitude, 10);
+
+            Address address = AddressList.FirstOrDefault();
+            return address;
+        }
+
+        void DisplayAddress(Address address)
+        {
+            if (address != null)
+            {
+                StringBuilder deviceAddress = new StringBuilder();
+                for (int i = 0; i < address.MaxAddressLineIndex; i++)
+                {
+                    deviceAddress.Append(address.GetAddressLine(i));
+                }
+                _addressText.Text = deviceAddress.ToString();
+            }
+            else
+            {
+                _addressText.Text = "Unable to determine the address. Try again Later!";
+            }
+        }
+
+     
+
     }
 }
 
--- AlertMe/Resources/Resource.Designer.cs
+++ AlertMe/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace AlertMe
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int location = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,8 +63,14 @@ namespace AlertMe
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int AdressTextBox = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int LocationImageButton = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int location_text = 2131034114;
 			
 			static Id()
 			{
